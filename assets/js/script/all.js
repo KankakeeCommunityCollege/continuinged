36ce39da503b1aeaebf8efabcb82da390d1febc3
wdzajicek <tig.welder4@gmail.com>
2019-07-02 01:40:34 -0500

    Create custom JS to AUTOMATICALLY highlight the current page's link in the navigation bar.

--- assets/js/script/all.js
+++ assets/js/script/all.js
@@ -1,4 +1,8 @@
 import contactLink from './contactLink.js';
+import highlightNav from './highlightCurrentNav.js';
 //import './test.js';
 
 contactLink();
+document.addEventListener('DOMContentLoaded', function() {
+  highlightNav();
+});
