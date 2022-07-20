3a0090a4bfd8497d19ba96bee8b60bcde86f8c2c
wdzajicek <tig.welder4@gmail.com>
2022-07-20 14:47:57 -0500

    Add comments where necessary.

--- assets/js/src/createJobFairTable.js
+++ assets/js/src/createJobFairTable.js
@@ -7,21 +7,32 @@ const API_PARAMS = {
 
 const SHEET_PARAMS = {
   spreadsheetId: '1Twj5nM1NazaAB5uu-NDNlX_Wv4OKIaDL05G1G3fTQOU',
-  range: 'ATTENDING EMPLOYERS!A:A'
+  range: 'ATTENDING EMPLOYERS!A:A' // A:A b/c we only need the first column which contains the company names
+}
+
+const DATATABLES_PARAMS = {
+  responsive: true, // Activate responsive powers GO!
+  paging: false, // Don't paginate. Should all be on one page
+  'order': []  // Disable initial column ordering
 }
 
 function fetchEmployers() {
   gapi.client.init(API_PARAMS).then(() => {
     return gapi.client.sheets.spreadsheets.values.get(SHEET_PARAMS);
   }).then((response) => {
-    createTableHtml(response);
+    let createTablePromise = new Promise(resolve => createTableHtml(response, resolve)) // Promise resolves after table HTML is built into page.
+
+    createTablePromise.then(PARENT => { // PARENT was passed to resolve(PARENT) in createTableHtml
+      const TABLE = PARENT.querySelector('#employerTable'); // TABLE is the first child of PARENT
+
+      $(TABLE).DataTable(DATATABLES_PARAMS); // DataTables() function is from <https://datatables.net/>
+    })
   }, function(err) { // Cannot be an arrow function
     console.error(`Error fetching Job Fair Employers: \n${err.result.error.message}`, err.result.error)
   })
 }
 
 function createJobFairTable() {
-  console.log(`createJobFairTable`);
   gapi.load('client', fetchEmployers);
 }
 
--- assets/js/src/createTableHtml.js
+++ assets/js/src/createTableHtml.js
@@ -1,9 +1,4 @@
 const PARENT = document.getElementById('attendingEmployers');
-const DATATABLES_PARAMS = {
-  responsive: true, // Activate responsive powers GO!
-  paging: false, // Don't paginate. Should all be on one page
-  'order': []  // Disable initial column ordering
-}
 
 function createTableRows(data) {
   let html = '';
@@ -15,30 +10,27 @@ function createTableRows(data) {
   return html;
 }
 
-function createTableHtml(response) {
-  const values = response.result.values;
-  const theadData = values.slice(1,2)
-  const tbodyData = values.slice(2);
-
+function createTableHtml(response, resolve) {
+  const VALUES = response.result.values; // values of a spreadsheet are always held under `response.result.values` if the response object
+  const THEAD_DATA = VALUES.slice(1, 2); // First row of spreadsheet values is the instructions row, second is our header row
+  const TBODY_DATA = VALUES.slice(2); // Everything else is data to process as the table body
+  // Since our data consists of a single column we can skip iterating over rows and access the first item (company name) directly
   const html = `<table id="employerTable" class="table table-striped table-hover">
   <thead>
     <tr>
       <th>
-        ${theadData[0]}
+        ${THEAD_DATA[0]}
       </th>
     </tr>
   </thead>
   <tbody>
-    ${createTableRows(tbodyData.sort())}
+    ${createTableRows(TBODY_DATA.sort())}
   </tbody>
 </table>`;
   
-  console.log(html);
   PARENT.innerHTML = '';
   PARENT.innerHTML = html;
-  const table = PARENT.querySelector('#employerTable');
-
-  $(table).DataTable(DATATABLES_PARAMS);
+  return resolve(PARENT); // PARENT is passed to resolve() so that it can be accessed in the .then() block
 }
 
 export default createTableHtml;
