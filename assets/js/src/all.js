ab07118fe40ee8276cc26bc828da946dc66f817a
wdzajicek <tig.welder4@gmail.com>
2023-09-12 15:16:02 -0500

    Ditch the `/career-services/job-fair/` page...
    * Migrate Job Fair page content into hiring events tab to
      consolidate all job fair information.
    * Delete Job Fair page.
    * Adjust when the `createJobFairTable` module gets
      dynamically imported:
      * Hold off on importing the module until after the
        tab has been opened because the table doesn't render
        correctly when the tab is closed (probably due to
        data-tables.)

--- assets/js/src/all.js
+++ assets/js/src/all.js
@@ -5,21 +5,40 @@ const MODAL_BUTTON_ID = 'modalButton';
 
 window.addEventListener('load', () => {
   if (document.getElementById(FORM_MODAL_ID) || document.getElementById(MODAL_BUTTON_ID)) {
-    import('./hideJotformModal').then(({default: hideJotformModalAfterLoad}) => hideJotformModalAfterLoad())
+    import('./hideJotformModal')
+      .then(({ default: hideJotformModalAfterLoad }) => hideJotformModalAfterLoad());
   }
   if (document.querySelector('.nav-tabs') || document.querySelector('.accordion')) {
-    import('./tabLink').then(({default: makeTabsLinkable}) => makeTabsLinkable())
+    import('./tabLink')
+      .then(({ default: makeTabsLinkable }) => makeTabsLinkable());
   }
   if (document.querySelector('#registrationWrapper')) {
-    import('./loadJotformOnModalOpen').then(({default: loadJotformOnModalOpen}) => loadJotformOnModalOpen())
+    import('./loadJotformOnModalOpen')
+      .then(({ default: loadJotformOnModalOpen }) => loadJotformOnModalOpen());
   }
   if (document.getElementById('attendingEmployers')) {
-    import('./createJobFairTable').then(({default: createJobFairTable}) => createJobFairTable())
+    /**
+     * @see https://getbootstrap.com/docs/4.6/components/navs/#events - for detecting Bootstrap 4 tab events
+     * @requires jQuery - Bootstrap 4 JS methods require jQuery's "$" to work
+     * @example $('#my-tab').on('shown.bs.tab', (e) => {
+     *             // Do something after the tab has been opened and animations are completed
+     *          });
+    */
+    // NOTE: A vanilla JS .addEventListener('click', ...) on the tab won't work because
+    //  tabs can be activated via hash-link URLs (e.g. `.../career-services/#hiring-events`)
+    $('#hiring-events-tab').on('shown.bs.tab', (e) => {
+      // DO NOT import and run this module until after the BS4 tab has been opened and animations completed.
+      //  Otherwise the table of employers does not render correctly — it gets squished and looks bad.
+      import('./createJobFairTable')
+        .then(({ default: createJobFairTable }) => createJobFairTable());
+    });
   }
   if (document.getElementById('JobBoard')) {
-    import('./createJobBoard').then(({ default: createJobBoard }) => createJobBoard());
+    import('./createJobBoard')
+      .then(({ default: createJobBoard }) => createJobBoard());
   }
   if (document.querySelector('[data-target="#staticBackdrop"]')) {
-    import('./focusDmiModal').then(({ default: focusDmiModal }) => focusDmiModal());
+    import('./focusDmiModal')
+      .then(({ default: focusDmiModal }) => focusDmiModal());
   }
 });
